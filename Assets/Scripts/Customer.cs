1b4e1ebc664a55091ff461d06daddef5cd85acfb
Peter <pjthompson.au@gmail.com>
2020-02-11 17:46:01 +1100

    Game

---
+++ Assets/Scripts/Customer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Customer : CustomerManager
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = spawnPos.position;
+        randomPath = Random.Range(1, 5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (randomPath == 1)
+        {
+            agent.SetDestination(pos1);
+        }
+        if (randomPath == 2)
+        {
+            agent.SetDestination(pos2);
+        }
+        if (randomPath == 3)
+        {
+            agent.SetDestination(pos3);
+        }
+        if (randomPath == 4)
+        {
+            agent.SetDestination(pos4);
+        }
+        if (randomPath == 5)
+        {
+            agent.SetDestination(pos5);
+        }
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject customer;
+    public Transform spawnPos;
+
+    private IEnumerator coroutine;
+
+    void Start()
+    {
+        
+        print("Starting " + Time.time);
+
+        coroutine = CustomerSpawn(10.0f);
+        StartCoroutine(coroutine);
+
+        print("Counting " + Time.time);
+    }
+
+    private IEnumerator CustomerSpawn(float waitTime)
+    {
+        while (true)
+        {
+            Instantiate(customer);
+            yield return new WaitForSeconds(waitTime);
+            print("Spawned Customer at the time of " + Time.time);
+        }
+        
+    }
+
+    void Update()
+    {
+        if (coroutine == CustomerSpawn(0.0f))
+        {
+            
+            CustomerSpawn(10.0f);
+        }
+    }
+}
