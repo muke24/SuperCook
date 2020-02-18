f4edd43a3d3357e8edde01da354abeb0cb3cf482
Peter <pjthompson.au@gmail.com>
2020-02-18 22:27:12 +1100

    stuff

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+	public Text gameTimeText;
+	public float gameTime = 60.0f;
+
+	void Update()
+	{
+		gameTimeText.text = "Time " + gameTime.ToString();
+
+		if (gameTime > 0f)
+		{
+			gameTime -= Time.deltaTime;
+		}
+
+		if (gameTime < 0f)
+		{
+			gameTime = 0f;
+		}
+
+	}
+}
